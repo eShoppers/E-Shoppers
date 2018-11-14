187fdcf23a30b3aa73c395b22741a7789b7485ec
salnok30 <salnok30@gmail.com>
2018-11-13 23:14:10 -0600

    catalog view and controller updated

--- src/main/java/edu/mum/cs425/project/eshoppers/controller/CatalogController.java
+++ src/main/java/edu/mum/cs425/project/eshoppers/controller/CatalogController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,5 +27,11 @@ public class CatalogController {
 			return null;
 		return catalogService.findAll();
 
+	}
+	
+	@GetMapping(value="/catalog")
+	public String cataloglist() {
+		
+		return "webapps/Catalog";
 	}
 }
--- src/main/java/edu/mum/cs425/project/eshoppers/controller/HomeController.java
+++ src/main/java/edu/mum/cs425/project/eshoppers/controller/HomeController.java
@@ -2,6 +2,7 @@ package edu.mum.cs425.project.eshoppers.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +16,15 @@ public class HomeController {
 	public String homepages() {
 		return "webapps/index";
 	}
-
+	@RequestMapping(value = "/contactus")
+	public String contactus() {
+		return "webapps/contactus";
+	}
+	
+	
+	
+	
+	
 //	@RequestMapping(value = "/login", method = RequestMethod.GET)
 //	public String login() {
 //		return "webapps/login";
